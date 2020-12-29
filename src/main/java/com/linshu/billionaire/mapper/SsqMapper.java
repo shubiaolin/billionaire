3ee032aa020c4436a7e8718bd5510d8b0bda37e2
shubiaolin <shubiaoyu@126.com>
2020-12-30 00:15:09 +0800

    SsqMapper.java 初步编写

--- src/main/java/com/linshu/billionaire/mapper/SsqMapper.java
+++ src/main/java/com/linshu/billionaire/mapper/SsqMapper.java
@@ -1,21 +1,26 @@
 package com.linshu.billionaire.mapper;
 
 import com.linshu.billionaire.entity.SsqEntity;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 @Mapper
 public interface SsqMapper {
-    //TODO selectAllList
-    //TODO selectById
-    //TODO selectByConditions
-    //TODO insert
-    //TODO insertList
-    //TODO update
-    //TODO updateList
-    //TODO delete
-    //TODO deleteById
-    //TODO deleteByConditions
-    List<SsqEntity> findAll();
+    List<SsqEntity> selectAllList();
+
+    SsqEntity selectById();
+
+    //TODO 待处理
+    List<SsqEntity> selectByConditions(SsqEntity condition);
+
+    Boolean insert(SsqEntity entity);
+    Boolean insertList(List<SsqEntity> entity);
+
+    Boolean update(SsqEntity entity);
+    Boolean updateList(List<SsqEntity> entity);
+
+    Boolean deleteById(long id);
+    Boolean deleteByConditions(SsqEntity entity);
 }
