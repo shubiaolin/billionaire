ef5f11adc7fe1fcbeec77d1f361e710e3b61deeb
shubiaolin <shubiaoyu@126.com>
2020-12-29 23:55:00 +0800

    连接500db数据库，生产entity

---
+++ src/main/java/com/linshu/billionaire/entity/SsqEntity.java
@@ -0,0 +1,186 @@
+package com.linshu.billionaire.entity;
+
+
+public class SsqEntity {
+
+  private long id;
+  private String numId;
+  private long red1;
+  private long red2;
+  private long red3;
+  private long red4;
+  private long red5;
+  private long red6;
+  private long blue;
+  private long redOrder1;
+  private long redOrder2;
+  private long redOrder3;
+  private long redOrder4;
+  private long redOrder5;
+  private long redOrder6;
+  private String openDate;
+  private String updateTime;
+  private Integer isDownload;
+
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+
+  public String getNumId() {
+    return numId;
+  }
+
+  public void setNumId(String numId) {
+    this.numId = numId;
+  }
+
+
+  public long getRed1() {
+    return red1;
+  }
+
+  public void setRed1(long red1) {
+    this.red1 = red1;
+  }
+
+
+  public long getRed2() {
+    return red2;
+  }
+
+  public void setRed2(long red2) {
+    this.red2 = red2;
+  }
+
+
+  public long getRed3() {
+    return red3;
+  }
+
+  public void setRed3(long red3) {
+    this.red3 = red3;
+  }
+
+
+  public long getRed4() {
+    return red4;
+  }
+
+  public void setRed4(long red4) {
+    this.red4 = red4;
+  }
+
+
+  public long getRed5() {
+    return red5;
+  }
+
+  public void setRed5(long red5) {
+    this.red5 = red5;
+  }
+
+
+  public long getRed6() {
+    return red6;
+  }
+
+  public void setRed6(long red6) {
+    this.red6 = red6;
+  }
+
+
+  public long getBlue() {
+    return blue;
+  }
+
+  public void setBlue(long blue) {
+    this.blue = blue;
+  }
+
+
+  public long getRedOrder1() {
+    return redOrder1;
+  }
+
+  public void setRedOrder1(long redOrder1) {
+    this.redOrder1 = redOrder1;
+  }
+
+
+  public long getRedOrder2() {
+    return redOrder2;
+  }
+
+  public void setRedOrder2(long redOrder2) {
+    this.redOrder2 = redOrder2;
+  }
+
+
+  public long getRedOrder3() {
+    return redOrder3;
+  }
+
+  public void setRedOrder3(long redOrder3) {
+    this.redOrder3 = redOrder3;
+  }
+
+
+  public long getRedOrder4() {
+    return redOrder4;
+  }
+
+  public void setRedOrder4(long redOrder4) {
+    this.redOrder4 = redOrder4;
+  }
+
+
+  public long getRedOrder5() {
+    return redOrder5;
+  }
+
+  public void setRedOrder5(long redOrder5) {
+    this.redOrder5 = redOrder5;
+  }
+
+
+  public long getRedOrder6() {
+    return redOrder6;
+  }
+
+  public void setRedOrder6(long redOrder6) {
+    this.redOrder6 = redOrder6;
+  }
+
+  public String getOpenDate() {
+    return openDate;
+  }
+
+  public void setOpenDate(String openDate) {
+    this.openDate = openDate;
+  }
+
+
+  public String getUpdateTime() {
+    return updateTime;
+  }
+
+  public void setUpdateTime(String updateTime) {
+    this.updateTime = updateTime;
+  }
+
+
+  public Integer getIsDownload() {
+    return isDownload;
+  }
+
+  public void setIsDownload(Integer isDownload) {
+    this.isDownload = isDownload;
+  }
+
+}
